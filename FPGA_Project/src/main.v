1f12f2238533aa37fef96f6623a0f1d629bfb6b9
ioComk <rw110407@gmail.com>
2020-01-09 14:35:30 +0900

    LED blinking by 8bit shift register

--- FPGA_Project/src/main.v
+++ FPGA_Project/src/main.v
@@ -2,33 +2,36 @@
 module snake_game_box (
     input CLK1_50,
 	input [9:0] SW,
-    output [9:0] LEDR
+    output [9:0] LEDR,
+	output [9:0] GPIO
 );
-    wire [25:0] max1 = 26'd49999999;
+    wire [25:0] max1 = 26'd6999999;
     wire en1hz;
 
     wire CLR1 = SW[0];
+	 
+	 reg clk = 0;
+	 reg cnt = 0;
 
-	reg [9:0] shift = 10'b0000000001;
+	 reg [8:0] shift = 9'b000000001;
 	
     ENCOUNT en1( .max(max1), .clk(CLK1_50), .CLR(CLR1), .en(en1hz) );
 
 	always @(posedge CLK1_50) begin
-        if(en1hz==1)
-            shift <= shift << 1;
-		  if(shift == 10'b0000000000)
-			   shift <= 10'b0000000001;
+        if(en1hz==1) begin
+				clk <= !clk;
+				cnt <= cnt + 1;
+				if (cnt % 2 == 0)
+					shift <= shift << 1;
+		  end
+
+		  if(shift == 9'b000000000)
+			   shift <= 9'b000000001;
     end
 	 
-	assign LEDR[0] = shift[0];
-	assign LEDR[1] = shift[1];
-	assign LEDR[2] = shift[2];
-	assign LEDR[3] = shift[3];
-	assign LEDR[4] = shift[4];
-	assign LEDR[5] = shift[5];
-	assign LEDR[6] = shift[6];
-	assign LEDR[7] = shift[7];
-	assign LEDR[8] = shift[8];
-	assign LEDR[9] = shift[9];
+	assign GPIO[0] = shift[0];
+
+	assign GPIO[4] = !clk;
+	assign GPIO[2] = clk;
      
 endmodule
