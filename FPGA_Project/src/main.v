a15edf033cf6fdef215da3b041c93ad0a4ad850a
ioComk <rw110407@gmail.com>
2020-01-19 12:26:59 +0900

    これがダイナミック点灯

--- FPGA_Project/src/main.v
+++ FPGA_Project/src/main.v
@@ -20,41 +20,41 @@ module snake_game_box (
 	reg isUp = 0, isDown = 1;
 
 	// シフトレジスタに入力するデータのインデクス
-	reg [6:0] outIndex = 0;
-	reg [6:0] rowIndex;
-
+	reg [6:0] outIdx = 0;
+	reg [6:0] rowIdx = 0;
 	// 1の部分が表示される
 	// 17bit目は画面外に出たとき用
-	reg [17-1:0] ser = 17'b0101001101101001;
-	reg [17-1:0] ser_col = 17'b00000000000000010;
+	reg [17-1:0] data[0:16-1];
+	reg [17-1:0] ser = 17'b00000000011000000;
+	reg [17-1:0] ser_col = 17'b00000000000000001;
+	reg tmp=0;
+	
+	initial begin
+		data[0][16:0]  = 17'b00000000000000000;
+		data[1][16:0]  = 17'b00000000000000000;
+		data[2][16:0]  = 17'b00000000000000000;
+		data[3][16:0]  = 17'b00000000000000000;
+		data[4][16:0]  = 17'b00000000000000000;
+		data[5][16:0]  = 17'b00000000000000000;
+		data[6][16:0]  = 17'b00000001110000000;
+		data[7][16:0]  = 17'b00000000000000000;
+		data[8][16:0]  = 17'b00000000000000000;
+		data[9][16:0]  = 17'b00000000000000000;
+		data[10][16:0] = 17'b00000000000000000;
+		data[11][16:0] = 17'b00000000000000000;
+		data[12][16:0] = 17'b00000000000000000;
+		data[13][16:0] = 17'b00000000000000000;
+		data[14][16:0] = 17'b00000000000000000;
+		data[15][16:0] = 17'b00000000000000000;
 
-//	always @* begin
-//		ser[0][16:0]  = 17'b01000000000011000;
-//		ser[1][16:0]  = 17'b00100000000000000;
-//		ser[2][16:0]  = 17'b01000000000000000;
-//		ser[3][16:0]  = 17'b00001000000000000;
-//		ser[4][16:0]  = 17'b00000100000000000;
-//		ser[5][16:0]  = 17'b00000010000000000;
-//		ser[6][16:0]  = 17'b00000001000000000;
-//		ser[7][16:0]  = 17'b00000000100000000;
-//		ser[8][16:0]  = 17'b00000000010000000;
-//		ser[9][16:0]  = 17'b00000000001000000;
-//		ser[10][16:0] = 17'b00000000001000000;
-//		ser[11][16:0] = 17'b00000000000100000;
-//		ser[12][16:0] = 17'b00000000000010000;
-//		ser[13][16:0] = 17'b00000000000001000;
-//		ser[14][16:0] = 17'b00000000000000100;
-//		ser[15][16:0] = 17'b00000000000000010;
-//		ser[16][16:0] = 17'b00000000000000000;
-//
-//		rowIndex <= 0;
-//	end
+		// ser = data[rowIdx][16:0];
+	end
 
 	//////////////////////////////////////////////////
 
  	// シフトレジスタに入力するクロックの周期を決める
 	// 50Mhz -> 1ms
-    wire [25:0] maxSr = 26'd499999;
+    wire [25:0] maxSr = 26'd499;
     wire enSr;
     ENCOUNT en1000( .max(maxSr), .clk(CLK1_50), .clr(!CLR), .en(enSr) );
 
@@ -77,13 +77,19 @@ module snake_game_box (
 				SRCLK = clk;
 			end
 		   	if ( cnt % 2 == 1 ) 
-		    	outIndex = outIndex + 1;
+		    	outIdx = outIdx + 1;
 
-			if( cnt > 32 ) begin
+			if( cnt > 31 ) begin
 				cnt = 0;
-		   end
-			if (RCLK == 1)
-				outIndex <= 0;
+				outIdx = 0;
+				rowIdx <= rowIdx + 1;
+				ser_col = ser_col << 1;
+				if (ser_col[16] == 1)
+					ser_col[0] <= 1;
+				// ser_col[0] = ser_col[16];
+	    	end
+			if (rowIdx > 15)
+				rowIdx <= 0;
 				
 			cnt = cnt + 1;
 		end
@@ -92,52 +98,64 @@ module snake_game_box (
 		if( !KEY[0] && KEY[1] ) begin
 			isUp <= 1;
 			isDown <= 0;
-			rowIndex <= 2;
 		end
 		else if( KEY[0] && !KEY[1]) begin
 			isUp <= 0;
 			isDown <= 1;
-			rowIndex <= 3;
 		end
 		else begin
 			isUp <= isUp;
 			isDown <= isDown;
-			// rowIndex <= 0;
 		end
 
-//		// 下方向に進む
-//		if( enSg && isUp && !isDown ) begin
-//				ser_col = ser_col << 1;
-//				ser_col[0] = ser_col[16];
-//				rowIndex <= rowIndex + 1;
-//				if(rowIndex >= 16)
-//					rowIndex <= 0;
-//		end
-//		// 上方向に進む
-//		if( enSg && isDown && !isUp ) begin
-//				ser_col = ser_col >> 1;
-//				ser_col[16] = ser_col[0];
-//				rowIndex <= rowIndex + 1;
-//				if(rowIndex >= 16)
-//					rowIndex <= 0;
-//		end
-    end	 
+		// 下方向に進む
+		if(enSr && isUp && !isDown ) begin
+			// data[rowIdx][16:0] = data[rowIdx][16:0] << 1;
+			// data[rowIdx][0] = data[rowIdx][16];
+		end
+		// 上方向に進む
+		if(enSr && isDown && !isUp ) begin
+			ser = ser >> 1;
+			ser[16] = ser[0];
+		end
+
+		// 能筋実装
+		case (rowIdx)
+			 0: ser = data[0][16:0]; 
+			 1: ser = data[1][16:0]; 
+			 2: ser = data[2][16:0]; 
+			 3: ser = data[3][16:0]; 
+			 4: ser = data[4][16:0]; 
+			 5: ser = data[5][16:0]; 
+			 6: ser = data[6][16:0]; 
+			 7: ser = data[7][16:0]; 
+			 8: ser = data[8][16:0]; 
+			 9: ser = data[9][16:0]; 
+			10: ser = data[10][16:0]; 
+			11: ser = data[11][16:0]; 
+			12: ser = data[12][16:0]; 
+			13: ser = data[13][16:0]; 
+			14: ser = data[14][16:0]; 
+			15: ser = data[15][16:0]; 
+			default: ser = ser; 
+		endcase
+	end
 
 	assign ARDUINO_IO[13] = CLR;
 	assign ARDUINO_IO[12] = RCLK;
 	assign ARDUINO_IO[11] = SRCLK;
 
-	assign ARDUINO_IO[9] = ser[outIndex % 16];
-	assign ARDUINO_IO[10]  = ser_col[outIndex % 16];
+	assign ARDUINO_IO[9] = ser[(outIdx-1) % 16];
+	assign ARDUINO_IO[10]  = ser_col[(outIdx-1) % 16];
 
 	// 確認用
 	assign LEDR[0] = isUp;
 	assign LEDR[1] = isDown;
-	
+	assign LEDR[3] = tmp;
 
-	assign LEDR[9] = SRCLK;
-	assign LEDR[8] = RCLK;
-	assign LEDR[6] = ser[outIndex % 16];	
-	// assign LEDR[5] = ser_col[outIndex % 16];
+	// assign LEDR[9] = SRCLK;
+	// assign LEDR[8] = RCLK;
+	// assign LEDR[6] = ser[(outIdx-1) % 16];	
+	// assign LEDR[5] = ser_col[(outIdx-1) % 16];
 
 endmodule 
